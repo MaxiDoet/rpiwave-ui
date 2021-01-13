ba9d5c2d20d6997b1551f1c69abacfed9bd7209f
TheHeroCraft1579 <maximilian.doetsch@outlook.de>
2021-01-13 13:30:58 +0000

    Added boot animation and standby screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,21 @@ var app = new Vue({
     data: {
     }
 })
+
+var appContainer = document.querySelector("#app")
+
+function setPage(pageNum) {
+    var c = appContainer.children;
+    var i;
+    for (i = 0; i < c.length; i++) {
+        if (c[i] != null && c[i].classList.contains("page")) {
+            c[i].classList.remove("active");
+        }        
+    }
+
+    if (c[pageNum]) {
+        c[pageNum].classList.add("active");
+    }
+}
+
+setPage(2);
