da080d6e8602a7c3a9b213225f11a15fcb516ca0
Ubuntu <ubuntu@maxirpi4.fritz.box>
2021-01-15 17:47:26 +0000

    Added latest view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        playerAnimatedBackgroundEnabled: true
+        playerAnimatedBackgroundEnabled: true,
+        latestAvaiable: false
     }
 })
 
